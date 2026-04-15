8ea6572b4ca110e3ba1c0d53acdab4abc120943d
SalohKhb <salohsoh2006@gmail.com>
2026-04-15 18:57:12 +0200

    update003

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  const headersList = headers();
-  const langHeader = headersList.get("accept-language") || "";
-
-  const lang = langHeader.split(",")[0]; // get first language
-
-  if (lang.startsWith("fr")) {
-    redirect("/fr");
-  }
-
-  if (lang.startsWith("ar")) {
-    redirect("/ar");
-  }
-
   redirect("/en");
 }
